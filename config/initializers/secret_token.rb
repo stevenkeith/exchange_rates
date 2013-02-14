9adf2db3c7a4ce33d0de5af3812b57aff5de1587
Steven Keith <stevenkeith85@gmail.com>
2013-02-14 09:36:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExchangeRates::Application.config.secret_token = 'ea880e02f5068d426b4de6d1995dfdf7ae882dc86d05f66588b2b4035f151524a7e7dfaaa5e219bf77ea9fdea1690b62225ea2d91b8f105942d9668f6565dfcb'
